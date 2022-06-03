a2ccc5e5953000e0aae1c75f4b1e4ebfb12cf172
Koala2122 <85144366+Koala2122@users.noreply.github.com>
2022-06-02 22:35:52 -0300

    mi primer comit

---
+++ EmpresaMecanica/EmpresaMecanica/Dominio/Persona.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpresaMecanica.Dominio
+{
+    internal class Persona
+    {
+        private int _cedula;
+        private string _nombre;
+        private string _telefono;
+
+        //Metodos de la clase
+
+        public int Cedula {
+            get {
+                return _cedula;
+            }
+
+            set
+            {
+
+                _cedula = value;
+
+            }
+
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                return _nombre;
+            }
+
+            set {
+
+                _nombre = value;
+
+            }
+
+
+        }
+
+        public string Telefono
+        {
+            get
+            {
+                return _telefono;
+            }
+
+            set
+            {
+
+                _telefono = value;
+
+            }
+
+
+        }
+
+        public override string ToString()
+        {
+            return this.Cedula + " " + this.Nombre + " " + this.Telefono;
+        }
+
+        public Persona(int pCedula, string pNombre, string pTelefono) {
+
+            this.Cedula = pCedula;
+            this.Nombre = pNombre;
+            this.Telefono = pTelefono;
+
+
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
